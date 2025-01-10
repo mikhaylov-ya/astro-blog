1c22bcc966f1dc3420bf3c7ff800f9e87c43c1e1
Yaroslav Mikhaylov <sponaproz@gmail.com>
2025-01-10 21:51:34 +0300

    chore: make single locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   i18n: {
     defaultLocale: "ru",
-    locales: ["ru", "en"],
-    routing: {
-      prefixDefaultLocale: false,
-    }
+    locales: ["ru"],
   },
 });
